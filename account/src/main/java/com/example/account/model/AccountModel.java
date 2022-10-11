86974ff51fb9aad172f605e74504b7d0818a625e
Baiwei Liu <s3778909@student.rmit.edu.au>
2022-10-11 17:20:04 +1100

    create the model for account function

---
+++ account/src/main/java/com/example/account/model/AccountModel.java
@@ -0,0 +1,66 @@
+@Entity
+@Table(name = "personModel")
+public class AccountModel {
+    @Id
+    private String accNumber;
+
+    @Column(name = "accountType")
+    private String accountType;
+    @Column(name = "accountName")
+    private String accountName;
+    @Column(name = "balance")
+    private String balance;
+    @Column(name = "date")
+    private Calendar date;
+
+    public AccountModel() {
+    }
+
+    public AccountModel(String accNumber, String accountType, String accountName, String balance, Calendar date) {
+        this.accNumber = accNumber;
+        this.accountType = accountType;
+        this.accountName = accountName;
+        this.balance = balance;
+        this.date = date;
+    }
+
+    public String getAccNumber() {
+        return accNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setAccNumber(String accNumber) {
+        this.accNumber = accNumber;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+}
